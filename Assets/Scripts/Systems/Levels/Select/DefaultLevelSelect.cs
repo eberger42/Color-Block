96d555e7473ad6f24c61479eec59315a5555f5b1
Erich Berger <erichb@controlconcepts.net>
2026-04-08 11:20:08 -0400

    Added Default Level Select for testing Puzzle Levels

---
+++ Assets/Scripts/Systems/Levels/Select/DefaultLevelSelect.cs
@@ -0,0 +1,43 @@
+﻿
+using Assets.Scripts.Systems.Data;
+using Assets.Scripts.Systems.LevelSelect;
+using UnityEngine;
+using static UnityEngine.Rendering.STP;
+
+namespace Assets.Scripts.Systems.Levels.Select
+{
+
+    [RequireComponent(typeof(LevelSelectManager))]
+    public sealed class DefaultLevelSelect : MonoBehaviour
+    {
+
+        private LevelSelectManager _levelSelectManager;
+        private ColorBlockGridDataAccessor _colorBlockGridDataAccessor;
+
+
+        private void Awake()
+        {
+            _levelSelectManager = GetComponent<LevelSelectManager>();
+
+
+          
+        }
+
+        void Start()
+        {
+            _colorBlockGridDataAccessor = ColorBlockGridDataAccessor.Instance;
+
+            var config = _colorBlockGridDataAccessor.GetColorBlockConfigurationDataByID("__Default");
+
+            var levelData = new LevelStateData()
+            {
+                levelName = config.name,
+                levelId = config.id,
+                unlocked = true,
+                completionStatus = false
+            };
+
+            _levelSelectManager.SelectLevel(new Level(levelData));
+        }
+    }
+}
